a5cdc0edd0923b4180c5fea6357e52078f929dfb
jedrzejdubial <jedrzejdubial@proton.me>
2024-08-18 00:17:48 +0200

    Created Main page

---
+++ components/Button.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps(['title', 'tag'])
+
+/* Takes tag prop and returns a custom element
+Made to work with PhosphorIcons */
+function render() {
+  return h(props.tag, {size: '32', color: 'white'})
+}
+</script>
+
+<template>
+    <div id="btnWrap">
+        <button id="btn">
+            <component :is="render" />
+        </button>
+        <p>{{ props.title }}</p>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,17 @@
+<script setup>
+import { PhFilePlus, PhFolderOpen } from '@phosphor-icons/vue'
+</script>
+
 <template>
-    <h1>Welcome to Truss Tool</h1>
+    <div id="wrap">
+        <h1>Welcome to Truss Tool</h1>
+        <div id="buttons">
+            <Button title="Create a new project" :tag="PhFilePlus" />
+            <Button title="Open an existing project" :tag="PhFolderOpen" />
+        </div>
+    </div>
 </template>
+
+<style>
+@import url("~/assets/css/styles.css");
+</style>
