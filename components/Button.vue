e582f45ab01eb0755b87b3b48b37879a387e67f5
jedrzejdubial <jedrzejdubial@proton.me>
2024-08-18 21:14:02 +0200

    Created 'New' page and added basic UI

--- components/Button.vue
+++ components/Button.vue
@@ -1,18 +1,39 @@
 <script setup>
-const props = defineProps(['title', 'tag'])
+const props = defineProps({
+  title: String,
+  tag: Object,
+  path: String,
+  action: Function
+})
 
 /* Takes tag prop and returns a custom element
 Made to work with PhosphorIcons */
-function render() {
-  return h(props.tag, {size: '32', color: 'white'})
-}
+const IconComponent = () => h(props.tag, { size: '32', color: 'white' })
 </script>
 
 <template>
-    <div id="btnWrap">
-        <button id="btn">
-            <component :is="render" />
+    <div id="button_wrap">
+        <button id="button_el" @click="navigateTo(props.path)">
+            <component :is="IconComponent" />
         </button>
         <p>{{ props.title }}</p>
     </div>
 </template>
+
+<style scoped>
+#button_wrap {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+#button_el {
+    width: 60px;
+    height: 60px;
+    padding: 15px;
+    border: none;
+    border-radius: 8px;
+    background-color: var(--gray);
+    cursor: pointer;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,10 @@ import { PhFilePlus, PhFolderOpen } from '@phosphor-icons/vue'
 </script>
 
 <template>
-    <div id="wrap">
+    <div id="main_wrap">
         <h1>Welcome to Truss Tool</h1>
-        <div id="buttons">
-            <Button title="Create a new project" :tag="PhFilePlus" />
+        <div id="main_buttons">
+            <Button title="Create a new project" :tag="PhFilePlus" :path="'/new'" />
             <Button title="Open an existing project" :tag="PhFolderOpen" />
         </div>
     </div>
@@ -14,4 +14,18 @@ import { PhFilePlus, PhFolderOpen } from '@phosphor-icons/vue'
 
 <style>
 @import url("~/assets/css/styles.css");
+
+#main_wrap {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border;
+}
+
+#main_buttons {
+    display: flex;
+    gap: 50px;
+}
 </style>
